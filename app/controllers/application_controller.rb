a03267f75927bd8ce49c545908c944f8bcf77e8b
Derek Kniffin <derek.kniffin@gmail.com>
2016-10-31 09:59:11 -0400

    Fix the tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render json: { name: "Bob" }
+    render json: { name: "Joe" }
   end
 end
